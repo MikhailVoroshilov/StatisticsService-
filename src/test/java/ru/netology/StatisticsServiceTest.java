60d941be33d95aac65644a5ac14398d3f54f13f8
Mikhail Voroshilov <m.s.voroshilovv@gmail.com>
2022-01-21 16:52:53 +0800

    test plus

--- src/test/java/ru/netology/StatisticsServiceTest.java
+++ src/test/java/ru/netology/StatisticsServiceTest.java
@@ -10,12 +10,22 @@ class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {0, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
+    @Test
+    void findMaxNext() {
+        StatisticsService service = new StatisticsService();
 
+        long[] incomesInBillions = {0, 7, 9, 14, 25, 103, 88, 65, 1, 51, 12};
+        long expected = 103;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
